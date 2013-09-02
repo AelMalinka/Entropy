a6cc9727906424a4b3389a9fa4f1c9ab995ac6c9
Michael Thomas (malinka) <aelmalinka@gmail.com>
2013-09-02 13:37:35 -0400

    add Application

---
+++ src/Application.hh
@@ -0,0 +1,27 @@
+/*	Copyright 2010 Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Leser General Public License v3
+	Application
+*/
+
+#if !defined ENTROPY_APPLICATION_INC
+#	define ENTROPY_APPLICATION_INC
+
+#	include "Exception.hh"
+
+	namespace Entropy {
+		class Application
+		{
+			public:
+				Application(int &, char *[]);
+				virtual ~Application();
+				virtual void operator() () = 0;
+			protected:
+				int &ArgC();
+				char **ArgV();
+			private:
+				int &_argc;
+				char **_argv;
+		};
+	}
+
+#endif
