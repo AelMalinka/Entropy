2be22ef49ac03a783237393a5700a27edb0773f6
Michael Thomas (malinka) <malinka@entropy-development.com>
2015-11-15 22:51:24 -0500

    PythonModule, tests implemented

---
+++ test/Module/python.cc
@@ -0,0 +1,32 @@
+/*	Copyright 2015 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Lesser General Public License v3
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include "Import.hh"
+
+#include "interface.hh"
+
+using namespace std;
+using namespace Entropy;
+
+int main()
+{
+	try
+	{
+		Import<::tests::Interface> a("module.py");
+
+		a->run();
+		(*a)["a"];
+		(*a)[0];
+
+		return EXIT_SUCCESS;
+	}
+	catch(exception &e)
+	{
+		cout << e << endl;
+	}
+
+	return EXIT_FAILURE;
+}
